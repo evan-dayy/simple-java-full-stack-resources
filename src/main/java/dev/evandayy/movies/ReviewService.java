e65e9b85d1bc3ef5bd315c843856126696aa824a
Evan Day <yichaoday_evan@berkeley.edu>
2023-04-19 22:40:47 -0700

    first method to update the entire database

--- src/main/java/dev/evandayy/movies/ReviewService.java
+++ src/main/java/dev/evandayy/movies/ReviewService.java
@@ -17,8 +17,7 @@ public class ReviewService {
         Review review = reviewRepository.insert(new Review(reviewBody));
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review))
-                .first();
+                .apply(new Update().push("reviewIds").value(review)).first();
         return review;
     }
 }
